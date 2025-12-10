b3d503cb84b5fd1bb8c8228c973cf5aa6289ce18
Cadis Hangoluan <hangoluancadis@gmail.com>
2025-12-10 10:24:21 +0700

    refactor(data): Reorganize platform-specific DI module
    This commit refactors the Koin dependency injection setup within the `core/data` module by moving the platform-specific module declaration.
    
    The `expect fun getPlatformDataModule()` has been extracted from `DataModule.kt` into its own file, `PlatformDataModule.kt`, in `commonMain`.
    
    The corresponding Android and iOS `actual` implementations have been renamed from `DataModule.android.kt` and `DataModule.ios.kt` to `PlatformDataModule.android.kt` and `PlatformDataModule.ios.kt` respectively, to align with the new structure.

--- core/data/src/commonMain/kotlin/com/core/data/di/DataModule.kt
+++ core/data/src/commonMain/kotlin/com/core/data/di/DataModule.kt
@@ -3,8 +3,6 @@ package com.core.data.di
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 
-expect fun getPlatformDataModule(): org.koin.core.module.Module
-
 @Module(includes = [NetworkModule::class])
 @ComponentScan("com.core.data")
 class DataModule
